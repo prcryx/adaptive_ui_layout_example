a9bb73f8eb1d04d3d9dbf62b2013dc9c9ff88375
prcryx <me.prc.fo@gmail.com>
2023-12-28 06:21:09 +0530

    updated to pub package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_responsive_layout/flutter_responsive_layout.dart';
+import 'package:adaptive_ui_layout/flutter_responsive_layout.dart';
 
 void main() {
   runApp(const MyApp());
